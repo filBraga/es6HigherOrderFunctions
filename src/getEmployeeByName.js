f1f977a13f82af60da03de856accbbcaea8a2c09
Filipe <filipe.b.braga@gmail.com>
2021-11-30 20:40:15 -0300

    3 exercicio

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  if (employeeName === undefined) { return {}; }
+  return data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
+};
 
 module.exports = getEmployeeByName;
